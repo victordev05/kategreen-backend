6e5bf8457e5b35e211d53b8653df4074c9a7113d
Victor Dev <victordev05@gmail.com>
2026-06-19 13:25:14 +0100

    kategreen backend ready

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const mongoose = require("mongoose");
-const app = require("./app");
-require("dotenv").config({ path: ".env" });
-console.log("MONGO_URI:", process.env.MONGO_URI);
+const app = require("./src/app");
+require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB connected");
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-})
-.catch((err) => console.log(err));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
